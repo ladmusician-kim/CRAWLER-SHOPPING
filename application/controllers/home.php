57235ae63cac881cc002fdfc1cd4377f674ffae7
김동진 <ladmusician.kim@gmail.com>
2015-08-04 14:01:54 +0900

    change code

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -14,18 +14,18 @@ class Home extends CI_Controller {
 
         $uri = 'http://innofun.com/mall/m_login.php';
 
-        //·Î±×ÀÎ Á¤º¸¸¦ ÀúÀåÇÒ ¹è¿­ auth¸¦ ¸¸µì´Ï´Ù
-        //¹è¿­ÀÇ key´Â ÇØ´ç Æû¿¡¼­ ³Ñ°ÜÁÙ nameÀÌ µÇ°Ú½À´Ï´Ù
+        //ï¿½Î±ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½è¿­ authï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ï´ï¿½
+        //ï¿½è¿­ï¿½ï¿½ keyï¿½ï¿½ ï¿½Ø´ï¿½ ï¿½ï¿½ ï¿½Ñ°ï¿½ï¿½ï¿½ nameï¿½ï¿½ ï¿½Ç°Ú½ï¿½ï¿½Ï´ï¿½
         $auth['email'] = 'goqual';
         $auth['password'] = 'rhznjfflxl1';
 
-        //½º´©ÇÇÀÇ submitÇÔ¼ö·Î ÆûÁ¤º¸¸¦ ³Ñ°ÜÁÝ½Ã´Ù
+        //ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ submitï¿½Ô¼ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ñ°ï¿½ï¿½Ý½Ã´ï¿½
         $snoopy->submit($uri, $auth);
 
-        //·Î±×ÀÎ¿¡ °ü·ÃÇÏ¿© ÄíÅ°¸¦ »ç¿ëÇÏ´Â °æ¿ì°¡ ÀÖÀ¸´Ï ÄíÅ°Á¤º¸¸¦ ÀúÀåÇØµÓ´Ï´Ù
+        //ï¿½Î±ï¿½ï¿½Î¿ï¿½ ï¿½ï¿½ï¿½ï¿½Ï¿ï¿½ ï¿½ï¿½Å°ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½Ï´ï¿½ ï¿½ï¿½ì°¡ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½Å°ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ØµÓ´Ï´ï¿½
         $snoopy->setcookies();
 
-        //ÀÌÁ¦ ·Î±×ÀÎ Á¤º¸¸¦ °¡Áö°í ÀÖÀ¸´Ï ´Ù½Ã uri·Î Á¢¼ÓÇØº¾½Ã´Ù
+        //ï¿½ï¿½ï¿½ï¿½ ï¿½Î±ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Ù½ï¿½ uriï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½Øºï¿½ï¿½Ã´ï¿½
         $snoopy->fetch($uri);
 
         var_dump($snoopy->results);
@@ -51,7 +51,7 @@ class Home extends CI_Controller {
                     $category = explode("<font", $split_str[15])[0];
                     $category = trim(explode("<b>", $category)[1]);
                     $label = explode("</a", $split_str[18])[0];
-                    $cost = explode("¿ø", $split_str[24])[0];
+                    $cost = explode("ï¿½ï¿½", $split_str[24])[0];
                     $img_split = $split_str[9]."\"/>";
                     $img_url = explode("./", $img_split);
 
@@ -71,6 +71,62 @@ class Home extends CI_Controller {
             }
             $index++;
         }
+    }
+
+    function test () {
+
+        $snoopy = new Snoopy;
+
+        //$this->__login($snoopy);
+
+        $snoopy->fetch('http://innofun.com/mall/m_mall_list.php?ps_ctid=03000000&ps_ctid_2=&ps_search=&ps_company=&ps_brand=&ps_min_money=&ps_max_money=&ps_line=&sType=m&sType=m&skinmode=&ps_page=1');
+
+        //var_dump($arr_split_by_href);
+
+        $split_table = '<table border="0" cellpadding="0" cellspacing="0" width="100%" align="center" style="border-bottom:1 solid #EFEFEF">';
+        $arr_split_by_table = explode($split_table, $snoopy->results);
+
+       // var_dump($arr_split_by_table[1]);
+
+        $split_for_href = '<a href="m_mall_detail.php';
+        $split_for_img_uri = 'src="';
+        $split_for_category = '<b>';
+        $split_for_category_1 = '<font';
+        $split_for_label = '">';
+
+        $idx = 0;
+
+        foreach($arr_split_by_table as $each) {
+            if ($idx != 0) {
+                $arr_split_by_href = explode($split_for_href, $each);
+
+                var_dump($arr_split_by_href);
+
+                $arr_for_category_label = explode('</b>', explode($split_for_category, $arr_split_by_href[2])[1]);
+
+                $img_uri = explode('" border',explode($split_for_img_uri, $arr_split_by_href[1])[1])[0];
+                $category = $arr_for_category_label[0];
+                $category_1 = explode($split_for_category_1, $arr_for_category_label[1])[0];
+                $label = explode("</a>", explode($split_for_label, $arr_split_by_href[3])[1])[0];
+
+                //$this->__print_debug($idx, $img_uri, $category .trim($category_1), $label);
+            }
+            $idx++;
+        }
+    }
+
+    function __print_debug ($idx, $img_uri, $category, $label) {
+        echo $idx ."\n";
+        echo '<p></p>';
+        var_dump($img_uri);
+        echo '<p></p>';
+        var_dump($category);
+        echo '<p></p>';
+        var_dump($label);
+
+        echo '<p></p>';
+        echo '<p></p>';
+        echo '<p></p>';
     }
 
     function __handle_db_product($category, $label, $price, $img_url) {
@@ -94,7 +150,7 @@ class Home extends CI_Controller {
     function __option ($split_str) {
         for($i = 32; $i < 38; $i++) {
             if(isset($split_str[$i])) {
-                if (strpos($split_str[$i], '-¿É¼Ç¼±ÅÃ-')) {
+                if (strpos($split_str[$i], '-ï¿½É¼Ç¼ï¿½ï¿½ï¿½-')) {
                     $option_idx = $i + 1;
                     while(isset($split_str[$option_idx]) && strpos($split_str[$option_idx], "</option>")) {
                         var_dump($split_str[$option_idx++]);
@@ -108,22 +164,16 @@ class Home extends CI_Controller {
     function __login($snoopy) {
         $uri = 'http://innofun.com/mall/m_login.php';
 
-        //·Î±×ÀÎ Á¤º¸¸¦ ÀúÀåÇÒ ¹è¿­ auth¸¦ ¸¸µì´Ï´Ù
-        //¹è¿­ÀÇ key´Â ÇØ´ç Æû¿¡¼­ ³Ñ°ÜÁÙ nameÀÌ µÇ°Ú½À´Ï´Ù
         $auth['login_id'] = 'goqual';
         $auth['login_pass'] = 'rhznjfflxl1';
 
-        //½º´©ÇÇÀÇ submitÇÔ¼ö·Î ÆûÁ¤º¸¸¦ ³Ñ°ÜÁÝ½Ã´Ù
         $snoopy->submit($uri, $auth);
 
-        //·Î±×ÀÎ¿¡ °ü·ÃÇÏ¿© ÄíÅ°¸¦ »ç¿ëÇÏ´Â °æ¿ì°¡ ÀÖÀ¸´Ï ÄíÅ°Á¤º¸¸¦ ÀúÀåÇØµÓ´Ï´Ù
         $snoopy->setcookies();
 
         $snoopy->fetch($uri);
         var_dump($snoopy->results);
     }
-
-
 
     function __print_data ($data) {
         echo $data['category'];
@@ -135,6 +185,28 @@ class Home extends CI_Controller {
         echo '<p></p>';
         echo '<p></p>';
         echo '<p></p>';
+    }
+
+    function login_test () {
+        $snoopy = new Snoopy();
+        $uri = 'http://innofun.com/mall/m_login_ok.php';
+
+        $auth['ps_ssl'] = 1;
+        $auth['morning_main_login[0].login_id'] = 'goqual';
+        $auth['login_pass'] = 'rhznjfflxl1';
+        $auth['morning_main_login.login_id'] = 'goqual';
+        $auth['morning_main_login.login_pass'] = 'rhznjfflxl1';
+        //$auth['ps_murl'] = 'https://innofun.com:455/mall/m_login_ok.php';
+        //$auth['url'] = 'index.php';
+
+        var_dump($auth);
+
+        $snoopy->submit($uri, $auth);
+
+        $snoopy->setcookies();
+
+        $snoopy->fetch($uri);
+        var_dump($snoopy->results);
     }
 
 }
